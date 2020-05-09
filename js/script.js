3b9425c40911f8431e0dce7fa1cc776f2fd1f7c3
Magdalena Świderska <m.swiderska@outlook.com>
2020-05-09 10:38:44 +0200

    Add href attribute& article search

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@
 });*/
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!')
   console.log(event);
+
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -16,7 +18,7 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
@@ -29,11 +31,23 @@ const titleClickHandler = function(event){
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+
+  console.log(articleSelector)
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+
+  console.log(targetArticle)
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
+  console.log('targetArticle:', targetArticle);
 
 }
 
